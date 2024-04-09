e2bb204711ef769172d511f9de914879946564f2
mk26710 <type-iv@mk26710.dev>
2024-04-10 01:41:36 +0400

    feat: added fallback images preload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
   import "@/styles/main.css";
+  import { onMount } from "svelte";
+
+  function preloadFallbacks() {
+    const portraitImg = new Image();
+    portraitImg.src = "/img/v1/portraits/undefined.webp";
+  }
+
+  onMount(() => {
+    preloadFallbacks();
+  });
 </script>
 
 <slot />
