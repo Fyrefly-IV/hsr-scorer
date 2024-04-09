bf7027a1eb201d5a2253560fa20440338babf928
mk26710 <type-iv@mk26710.dev>
2024-04-10 01:44:05 +0400

    feat: added CharacterPortrait component

---
+++ src/components/CharacterPortrait.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { Character } from "@/data/characters";
+
+  export let character: Character;
+
+  $: portraitPath = character.portraitPath ?? "/img/v1/portraits/undefined.webp";
+</script>
+
+<div class="relative max-w-[300px]">
+  <img
+    style="image-rendering: pixelated;"
+    src={portraitPath}
+    alt="{character.name} portrait"
+    class="aspect-[9/16] h-[300px] object-cover"
+  />
+</div>
