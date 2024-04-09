3cd45e7090785eb780c8db4a559591bcb954b770
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-04-10 01:44:56 +0400

    feat: moved full quiz into a complete quiz mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,70 +1,21 @@
-<script lang="ts">
-  import { ComparisonGraph } from "@/data/characters";
-  import { onMount } from "svelte";
-  import { randomElement } from "$lib/arrays";
+<main class="flex min-h-screen w-full flex-col items-center justify-center">
+  <div class="flex flex-col items-center">
+    <h1 class="font-anuphan text-5xl font-bold">Honkai Scorer</h1>
+    <p class="text-sm mt-1">Find out which characters you like the most</p>
+  </div>
 
-  const scores: Record<string, { character: string; value: number; changedAt: number }> = {};
-  $: results = Object.values(scores).toSorted((a, b) => b.value - a.value);
-
-  let comparisonGraph = new ComparisonGraph();
-  let notConnectedNodes = comparisonGraph.findNonAdjacentPairs();
-
-  $: question = randomElement(notConnectedNodes) ?? [];
-
-  function incrementScore(character: string): void {
-    const entry = scores[character];
-
-    const value = (entry?.value ?? 0) + 1;
-    const changedAt = new Date().getTime();
-
-    scores[character] = { character, changedAt, value };
-  }
-
-  function chooseCharacter(chosenCharacter: string, otherCharacter: string): void {
-    if (!Array.isArray(question)) {
-      return;
-    }
-
-    incrementScore(chosenCharacter);
-    comparisonGraph.connect(chosenCharacter, otherCharacter);
-    notConnectedNodes = comparisonGraph.findNonAdjacentPairs();
-  }
-</script>
-
-<main
-  class="flex min-h-screen w-full flex-col items-center justify-center bg-zinc-900 text-zinc-200"
->
-  {#if notConnectedNodes.length > 0}
-    <h4 class="mb-4 text-center text-zinc-600">
-      {notConnectedNodes.length} pairs left
-    </h4>
-  {/if}
-
-  <div class="flex gap-10">
-    {#if question.length > 1}
-      <div class="flex flex-col items-center">
-        <h3 class="font-anuphan text-2xl font-bold">{question[0]}</h3>
-        <button class="" on:click={() => chooseCharacter(question[0], question[1])}>
-          i choose you
-        </button>
-      </div>
-
-      <div class="flex flex-col items-center">
-        <h3 class="font-anuphan text-2xl font-bold">{question[1]}</h3>
-        <button on:click={() => chooseCharacter(question[1], question[0])}>i choose you</button>
-      </div>
-    {:else}
-      <div class="flex flex-col">
-        <ol class="list-decimal">
-          {#each results as entry (entry.character)}
-            <li class="font-anuphan text-lg">
-              <span class="font-bold">{entry.character}</span>
-              <span> was chosen </span>
-              <span class="font-bold">{entry.value} times</span>
-            </li>
-          {/each}
-        </ol>
-      </div>
-    {/if}
+  <div class="grid grid-cols-1 grid-rows-2 gap-4 md:grid-cols-2 md:grid-rows-1 mt-8">
+    <a
+      href="/complete"
+      class="flex items-center justify-center rounded-md bg-white/5 px-4 py-2 text-center font-anuphan text-2xl font-bold duration-150 hover:bg-white/10 disabled:pointer-events-none disabled:opacity-50"
+    >
+      Complete Mode
+    </a>
+    <button
+      class="flex items-center justify-center rounded-md bg-white/5 px-4 py-2 text-center font-anuphan text-2xl font-bold duration-150 hover:bg-white/10 disabled:pointer-events-none disabled:opacity-50"
+      disabled
+    >
+      Tournament Mode
+    </button>
   </div>
 </main>
