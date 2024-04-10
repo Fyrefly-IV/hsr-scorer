6010c7b582cb16ad74788cb41ce624289e722355
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-04-10 17:52:01 +0400

    feat: layout now has very basic navigation on top left corner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
-  import "@/styles/main.css";
+  import HomeMiniIcon from "@/components/icons/mini/HomeMiniIcon.svelte";
+  import Cog6ToothMiniIcon from "@/components/icons/mini/Cog6ToothMiniIcon.svelte";
   import { onMount } from "svelte";
+
+  import "@/styles/main.css";
 
   function preloadFallbacks() {
     const portraitImg = new Image();
@@ -11,5 +14,21 @@
     preloadFallbacks();
   });
 </script>
+
+<div class="absolute left-0 top-0 z-10 ml-4 mt-4 flex flex-row gap-4">
+  <a
+    href="/"
+    class="group flex aspect-square w-8 items-center justify-center rounded bg-black/30 opacity-75 duration-150 hover:opacity-100"
+  >
+    <HomeMiniIcon />
+  </a>
+
+  <a
+    href="/config"
+    class="group flex aspect-square w-8 items-center justify-center rounded bg-black/30 opacity-75 duration-150 hover:opacity-100"
+  >
+    <Cog6ToothMiniIcon />
+  </a>
+</div>
 
 <slot />
