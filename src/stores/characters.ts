56d9bf7a21b7e2c28867607f9890fb40581d1a1a
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-04-14 02:37:58 +0400

    feat: added persistance to excluded characters store

--- src/stores/characters.ts
+++ src/stores/characters.ts
@@ -1,4 +1,36 @@
-import type { Character } from "@/data/characters";
+import { isCharacterArray, type Character } from "@/data/characters";
 import { writable, type Writable } from "svelte/store";
+import { browser } from "$app/environment";
 
-export const ExcludedCharacters: Writable<Character[]> = writable([]);
+const STORAGE_KEY = "excluded-characters";
+
+export const ExcludedCharacters: Writable<Character[]> = writable(getInitialState());
+
+ExcludedCharacters.subscribe((value) => localStorage.setItem(STORAGE_KEY, JSON.stringify(value)));
+
+function getInitialState(): Character[] {
+  if (!browser) {
+    return [];
+  }
+
+  const savedRawData = localStorage.getItem(STORAGE_KEY);
+  if (savedRawData == null) {
+    return [];
+  }
+
+  const savedJsonData = JSON.parse(savedRawData);
+  if (!Array.isArray(savedJsonData)) {
+    return [];
+  }
+
+  // there's no way user can have more than 100 entries
+  if (savedJsonData.length > 100) {
+    return [];
+  }
+
+  if (!isCharacterArray(savedJsonData)) {
+    return [];
+  }
+
+  return savedJsonData;
+}
