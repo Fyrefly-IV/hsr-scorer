9ab8421fe8e15accf9d1292edd0d559db6957d54
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-04-15 02:29:55 +0400

    feat: added combinations and cleaned up arrays algorithms

--- src/lib/arrays.ts
+++ src/lib/arrays.ts
@@ -1,38 +1,53 @@
-export const randomElement = <T>(arr: T[]): T | null => {
+export function randomElement<T>(arr: T[]): T | null {
   if (arr.length === 0) {
     return null;
   }
 
   const randomIndex = Math.floor(Math.random() * arr.length);
   return arr[randomIndex];
-};
+}
 
-export const randomElements = <T>(arr: T[], count: number = 1): T[] | null => {
+export function randomElements<T>(arr: T[], count: number = 1): T[] | null {
   if (arr.length < count) {
     return null;
   }
 
   const shuffledArr = arr.slice().sort(() => Math.random() - 0.5);
   return shuffledArr.slice(0, count);
-};
+}
 
-export const permutations = <T>(arr: T[], k: number): T[][] => {
-  const result: T[][] = [];
+// https://blog.lublot.dev/combinations-in-typescript
+export function combinations<T>(arr: T[], comboSize: number = arr.length): T[][] {
+  const combinations: T[][] = [];
+  const stack: number[] = [];
+  let i = 0;
 
-  const backtrack = (start: number, current: T[]): void => {
-    if (current.length === k) {
-      result.push([...current]);
-      return;
+  comboSize = Math.min(arr.length, comboSize);
+
+  while (stack.length > 0 || i < arr.length) {
+    if (stack.length === comboSize) {
+      combinations.push(stack.map((index) => arr[index]));
+      i = stack.pop()! + 1;
     }
 
-    for (let i = start; i < arr.length; i++) {
-      current.push(arr[i]);
-      backtrack(i + 1, current);
-      current.pop();
+    if (i >= arr.length) {
+      if (stack.length === 0) {
+        break;
+      }
+      i = stack.pop()! + 1;
+    } else {
+      stack.push(i++);
     }
-  };
+  }
 
-  backtrack(0, []);
+  return combinations;
+}
 
-  return result;
-};
+
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+export function shuffleArray<T>(array: T[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
