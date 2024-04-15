62d4e89a6414ad6e14647f7e5c1fe4e5b6792864
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-04-16 00:35:58 +0400

    feat: complete mode now can be saved to local storage

--- src/stores/complete-mode-quiz.ts
+++ src/stores/complete-mode-quiz.ts
@@ -1,37 +1,42 @@
-import { CHARACTERS, type Character } from "@/data/characters";
+import { CHARACTERS, CharacterSchema, type Character } from "@/data/characters";
 import { combinations, shuffleArray } from "$lib/arrays";
 import { excludedCharacterIds } from "./characters";
 import { get, writable } from "svelte/store";
+import { z } from "zod";
+import { browser } from "$app/environment";
 
-type ScoreEntry = {
-  characterId: Character["id"];
-  value: number;
-};
+const STORAGE_KEY = "hsr-scorer-complete-mode-quiz";
 
-type HistoryEntry = {
-  createdAt: number;
-  pair: [Character["id"], Character["id"]];
-  winnerId: Character["id"];
-};
+const ScoreEntrySchema = z.object({
+  characterId: CharacterSchema.shape.id,
+  value: z.number(),
+});
 
-type CompleteModeState = {
-  started: boolean;
-  queue: Character["id"][][];
-  scores: { [key: Character["id"]]: ScoreEntry };
-  choiceHistory: HistoryEntry[];
-};
+const HistoryEntrySchema = z.object({
+  createdAt: z.number().positive(),
+  pair: z.tuple([CharacterSchema.shape.id, CharacterSchema.shape.id]),
+  winnerId: CharacterSchema.shape.id,
+});
 
-const createEmptyState = (): CompleteModeState => ({
+const StateSchema = z.object({
+  started: z.boolean(),
+  queue: z.tuple([CharacterSchema.shape.id, CharacterSchema.shape.id]).array(),
+  scores: z.record(CharacterSchema.shape.id, ScoreEntrySchema),
+  choiceHistory: HistoryEntrySchema.array(),
+});
+
+type Store = z.infer<typeof StateSchema>;
+
+const createEmptyState = (): Store => ({
   started: false,
   queue: [],
   scores: {},
   choiceHistory: [],
 });
 
-const createState = (): CompleteModeState => {
-  console.debug("creating state for complete mode");
-
-  const scores: CompleteModeState["scores"] = {};
+const createState = (): Store => {
+  console.debug("[COMPLETE MODE STORE] creating state for complete mode");
+  const scores: Store["scores"] = {};
 
   for (const ch of CHARACTERS) {
     if (get(excludedCharacterIds).includes(ch.id)) {
@@ -44,18 +49,36 @@ const createState = (): CompleteModeState => {
     };
   }
 
-  const queue = combinations(
-    Object.values(scores).map((entry) => entry.characterId),
-    2,
-  );
+  const poolIds = Object.values(scores).map((entry) => entry.characterId);
+  const queue = combinations(poolIds, 2) as [Character["id"], Character["id"]][];
 
   shuffleArray(queue);
 
   return { queue, scores, choiceHistory: [], started: true };
 };
 
+const createInitialState = (): Store => {
+  if (browser) {
+    const maybeSavedData = localStorage.getItem(STORAGE_KEY);
+
+    if (maybeSavedData != null) {
+      const maybeJson = JSON.parse(maybeSavedData);
+      console.debug("[COMPLETE MODE STORE] detected saved complete mode state, parsing...");
+      const parseResult = StateSchema.safeParse(maybeJson);
+      if (parseResult.success) {
+        console.debug("[COMPLETE MODE STORE] saved state parsed and returned");
+        return parseResult.data;
+      } else {
+        console.error(parseResult.error);
+      }
+    }
+  }
+
+  return createEmptyState();
+};
+
 export function createCompleteModeQuiz() {
-  const { set, update, subscribe } = writable<CompleteModeState>(createEmptyState());
+  const { set, update, subscribe } = writable<Store>(createInitialState());
 
   const create = () => {
     set(createState());
@@ -118,3 +141,9 @@ export function createCompleteModeQuiz() {
 }
 
 export const completeModeQuiz = createCompleteModeQuiz();
+
+completeModeQuiz.subscribe((store) => {
+  if (browser) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(store));
+  }
+});
