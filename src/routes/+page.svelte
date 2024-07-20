ed2a8fc9e711729bf9f1f135ff91fd6a21e56b23
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-07-21 00:37:13 +0400

    feat: hide tournament mode button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,18 +4,12 @@
     <p class="text-sm mt-1">Find out which characters you like the most</p>
   </div>
 
-  <div class="grid grid-cols-1 grid-rows-2 gap-4 md:grid-cols-2 md:grid-rows-1 mt-8">
+  <div class="grid grid-cols-1 grid-rows-1 gap-4 md:grid-rows-1 mt-8">
     <a
       href="/complete"
       class="flex items-center justify-center rounded-md bg-white/5 px-4 py-2 text-center font-anuphan text-2xl font-bold duration-150 hover:bg-white/10 disabled:pointer-events-none disabled:opacity-50"
     >
-      Complete Mode
+      Complete Quiz 
     </a>
-    <button
-      class="flex items-center justify-center rounded-md bg-white/5 px-4 py-2 text-center font-anuphan text-2xl font-bold duration-150 hover:bg-white/10 disabled:pointer-events-none disabled:opacity-50"
-      disabled
-    >
-      Tournament Mode
-    </button>
   </div>
 </main>
