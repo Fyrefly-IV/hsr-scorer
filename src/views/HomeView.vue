c9abae8a9e8612ef891082c2313179c097da802b
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-07-24 01:46:53 +0400

    feat: init vue + vite

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useCounterStore } from "@/stores/counter";
+
+const counter = useCounterStore();
+</script>
+
+<template>
+  <main class="flex flex-col items-center justify-center min-h-screen">
+    <h2 class="text-lg font-black">{{ counter.count }}</h2>
+
+    <button
+      @click="counter.increment"
+      class="bg-neutral-200 text-neutral-800 cursor-pointer px-4 py-2"
+    >
+      increment
+    </button>
+  </main>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,59 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 
+import { defineConfig, HtmlTagDescriptor } from "vite";
+import type { Plugin } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [vue(), vueJsx(), injectCssAsStyleTag(), removeStylesheetLinks()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    cssCodeSplit: false,
+  },
 });
+
+function injectCssAsStyleTag(): Plugin {
+  return {
+    name: "inject-css-as-style-tags",
+    enforce: "post",
+    apply: "build",
+    transformIndexHtml(html, ctx) {
+      const htmlTagDescriptors: HtmlTagDescriptor[] = [];
+      const bundle = ctx.bundle;
+      if (bundle == null) {
+        return [];
+      }
+
+      Object.values(bundle)
+        .filter((output) => output.fileName.endsWith(".css"))
+        .forEach((output) => {
+          if (output.type === "asset" && typeof output.source === "string") {
+            htmlTagDescriptors.push({
+              tag: "style",
+              children: output.source,
+              injectTo: "head",
+            });
+          }
+        });
+
+      return htmlTagDescriptors;
+    },
+  };
+}
+
+function removeStylesheetLinks(): Plugin {
+  return {
+    name: "remove-stylesheet-links",
+    enforce: "post",
+    apply: "build",
+    transformIndexHtml(html, ctx) {
+      return html.replaceAll(/<link\s+rel="stylesheet"(\s.*\s)href="(.*)\.css">/gi, "");
+    },
+  };
+}
