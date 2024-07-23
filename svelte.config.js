c7732223ceb2d9508533dc51d5127efb52956732
mk26710 <type-iv@mk26710.dev>
2024-07-24 01:29:42 +0400

    style: minor styling changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
@@ -14,7 +14,7 @@ const config = {
     adapter: adapter(),
 
     alias: {
-      "@/*": "src/*"
+      "@/*": "src/*",
     },
   },
 };
