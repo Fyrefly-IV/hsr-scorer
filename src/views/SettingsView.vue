45a46928330b9061bef2a9d0866db73c64ed2105
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-07-24 20:20:23 +0400

    feat: settings page allows selecting and deselecting all characters from pool

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import CharacterCard from "@/components/CharacterCard.vue";
 import Main from "@/components/Main.vue";
+import H1 from "@/components/typography/H1.vue";
+import P from "@/components/typography/P.vue";
+import Button from "@/components/ui/Button.vue";
 import { CHARACTERS, type StarRailCharacter } from "@/data/characters";
 import { useSettingsStore } from "@/stores/settings";
 
@@ -8,33 +11,50 @@ const settings = useSettingsStore();
 
 const characterClickHandler = (id: StarRailCharacter["id"]) => {
   if (!settings.isExcludedId(id)) {
-    settings.excludeId(id);
+    settings.excludeIds(id);
   } else {
-    settings.includeId(id);
+    settings.includeIds(id);
   }
+};
+
+const handleStarRailIncludeAll = () => {
+  settings.includeIds(...CHARACTERS.map((c) => c.id));
+};
+
+const handleStarRailExcludeAll = () => {
+  settings.excludeIds(...CHARACTERS.map((c) => c.id));
 };
 </script>
 
 <template>
   <Main class="flex flex-col">
-    <div class="font-anuphan container mt-10">
-      <h1 class="text-2xl md:text-4xl font-bold">Settings</h1>
-      <p class="text-lg">
+    <div class="container mt-10 font-anuphan">
+      <H1>Settings</H1>
+      <P class="text-lg">
         In settings you can pick characters which you want to be present in your comparisons by
         simply clicking on them!
-      </p>
+      </P>
     </div>
 
-    <div class="container mt-8 flex flex-row flex-wrap justify-evenly gap-8">
-      <CharacterCard
-        v-for="c in CHARACTERS"
-        :key="c.id"
-        :character="c"
-        @click="() => characterClickHandler(c.id)"
-        class="cursor-pointer"
-        :excluded="settings.isExcludedId(c.id)"
-        hoverable
-      />
+    <div class="container mt-10">
+      <div>
+        <H1>Star Rail</H1>
+        <div class="mt-4 flex flex-row gap-2">
+          <Button @click="handleStarRailIncludeAll">Select All</Button>
+          <Button @click="handleStarRailExcludeAll">Deselect All</Button>
+        </div>
+      </div>
+      <div class="mt-6 flex flex-row flex-wrap justify-evenly gap-8">
+        <CharacterCard
+          v-for="c in CHARACTERS"
+          :key="c.id"
+          :character="c"
+          @click="() => characterClickHandler(c.id)"
+          class="cursor-pointer"
+          :excluded="settings.isExcludedId(c.id)"
+          hoverable
+        />
+      </div>
     </div>
   </Main>
 </template>
