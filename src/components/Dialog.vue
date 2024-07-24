6568c229cc389d9a08f11c6e7f8487ad4e14a373
mk26710 <type-iv@mk26710.dev>
2024-07-24 23:35:14 +0400

    feat: dialog component

---
+++ src/components/Dialog.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { cn } from "@/lib/cn";
+import { vOnClickOutside } from "@vueuse/components";
+import { effect, ref, watch } from "vue";
+
+const open = defineModel<boolean>({ required: true });
+const dialog = ref<HTMLDialogElement>();
+
+const close = () => {
+  open.value = false;
+};
+
+effect(() => {
+  if (open.value && dialog.value) {
+    dialog.value.showModal();
+  }
+});
+
+watch(
+  () => open.value,
+  (s) => {
+    if (s === false) {
+      dialog.value?.close();
+    }
+  },
+);
+</script>
+
+<template>
+  <dialog :open="open" ref="dialog">
+    <div
+      v-if="open === true"
+      v-on-click-outside="close"
+      :class="
+        cn(
+          'flex w-full max-w-[600px] flex-col rounded-md border border-neutral-700 bg-neutral-800 p-4 text-neutral-200',
+        )
+      "
+    >
+      <slot></slot>
+    </div>
+  </dialog>
+</template>
+
+<style lang="css">
+dialog::backdrop {
+  background: rgba(0, 0, 0, 0.5);
+}
+
+dialog[open] {
+  animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+}
+
+@keyframes zoom {
+  from {
+    transform: scale(0.95);
+  }
+  to {
+    transform: scale(1);
+  }
+}
+
+dialog[open]::backdrop {
+  animation: fade 0.2s ease-out;
+}
+
+@keyframes fade {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
