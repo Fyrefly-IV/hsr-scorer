a8580c9903534534961a4d8b54c1b485af39a772
mk26710 <type-iv@mk26710.dev>
2024-07-24 20:18:10 +0400

    feat: typography components

---
+++ src/components/typography/Blockquote.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { cn } from "@/lib/cn";
+import type { ClassValue } from "clsx";
+</script>
+
+<template>
+  <blockquote :class="cn('mt-6 border-l-2 pl-6 italic', $attrs.class as ClassValue)">
+    <slot></slot>
+  </blockquote>
+</template>
