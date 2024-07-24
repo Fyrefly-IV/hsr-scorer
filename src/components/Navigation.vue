e06157202664e672c04586883e7f67a64b4b6c96
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-07-24 18:20:00 +0400

    feat: background for nav buttons

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -22,7 +22,7 @@ const nav = [
       v-for="endpoint in nav"
       :key="endpoint.href"
       :to="endpoint.href"
-      class="flex h-9 items-center justify-center rounded-md border box-border border-neutral-800 px-3 transition-all duration-200 ease-in-out hover:bg-neutral-800"
+      class="box-border flex h-9 items-center justify-center rounded-md border border-neutral-800 bg-neutral-900 px-3 transition-all duration-200 ease-in-out hover:bg-neutral-800"
     >
       <component :is="endpoint.icon" />
     </RouterLink>
