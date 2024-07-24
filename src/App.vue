e2e3a862271460abf4e38e55bd33af96741fa158
mk26710 <type-iv@mk26710.dev>
2024-07-24 18:13:10 +0400

    feat: navigation for the app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import Navigation from "@/components/Navigation.vue";
 </script>
 
 <template>
+  <Navigation />
   <RouterView />
 </template>
