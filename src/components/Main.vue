8cd0c9c492edee91bb8af94d2ecbaf5b8bf8924c
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-07-24 16:44:25 +0400

    feat: main element wrapper component

---
+++ src/components/Main.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { cn } from "@/lib/cn";
+import type { ClassValue } from "clsx";
+</script>
+
+<template>
+  <main :class="cn('min-h-screen p-4', $attrs.class as ClassValue)">
+    <slot></slot>
+  </main>
+</template>
