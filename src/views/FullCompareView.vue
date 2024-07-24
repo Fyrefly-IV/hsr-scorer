ca1dcc9760b1136e97772547218eecd489bca54e
mk26710 <type-iv@mk26710.dev>
2024-07-25 01:40:37 +0400

    feat: display results

--- src/views/FullCompareView.vue
+++ src/views/FullCompareView.vue
@@ -6,15 +6,41 @@ import H1 from "@/components/typography/H1.vue";
 import H2 from "@/components/typography/H2.vue";
 import P from "@/components/typography/P.vue";
 import Button from "@/components/ui/Button.vue";
-import type { StarRailCharacter } from "@/data/characters";
+import { characterById, type StarRailCharacter } from "@/data/characters";
 import { useFullModeStore } from "@/stores/full-compare";
 import { useThrottleFn } from "@vueuse/core";
 import { computed, ref } from "vue";
 
 const fullMode = useFullModeStore();
-const modalOpen = ref<boolean>(false);
+const showModal = ref<boolean>(false);
+const showScores = ref<boolean>(false);
 
 const canUndo = computed(() => fullMode.choices.length > 0);
+
+const scores = computed(() => {
+  const scoresSorted = Object.entries(fullMode.scores);
+  scoresSorted.sort((a, b) => b[1] - a[1]);
+
+  const scoresWithCharacters = scoresSorted.map(([id, score]) => {
+    const character = characterById(id);
+    if (character == null) {
+      throw TypeError("character with score was not found");
+    }
+
+    return { score, character };
+  });
+
+  return scoresWithCharacters;
+});
+
+const winners = computed(() => {
+  if (scores.value.length === 0) {
+    return [];
+  }
+
+  const bestScore = scores.value[0].score;
+  return scores.value.filter((entry) => entry.score === bestScore);
+});
 
 const start = () => {
   fullMode.start();
@@ -31,17 +57,18 @@ const undo = () => {
 };
 
 const confirmReset = () => {
-  modalOpen.value = true;
+  showModal.value = true;
 };
 
 const reset = () => {
-  modalOpen.value = false;
+  showModal.value = false;
+  showScores.value = false;
   fullMode.reset();
 };
 </script>
 
 <template>
-  <Main class="flex items-center justify-center">
+  <Main class="flex w-full items-center justify-center py-14">
     <div v-if="fullMode.screen === 'start'" class="flex max-w-[800px] flex-col">
       <H1 class="text-center">Full Compare</H1>
       <P class="text-justify">
@@ -82,9 +109,47 @@ const reset = () => {
         <Button variant="secondary" @click="confirmReset">Stop</Button>
       </div>
     </div>
+
+    <div v-if="fullMode.screen === 'results'" class="flex w-full flex-col items-center">
+      <div class="flex max-w-[800px] flex-col">
+        <H2 class="mb-4 text-center font-bold">
+          Most chosen character<span v-if="winners.length > 1">s</span>
+        </H2>
+        <div class="mb-4 flex flex-row flex-wrap justify-center">
+          <CharacterCard
+            v-for="(entry, i) in winners"
+            :key="`winner-${i}`"
+            :character="entry.character"
+            class="w-[250px]"
+          />
+        </div>
+
+        <Button variant="secondary" @click="confirmReset" class="w-fit self-center">Reset</Button>
+
+        <button
+          class="mt-6 w-fit self-center rounded-md px-2 py-1 text-sm text-neutral-600 hover:bg-neutral-700 hover:text-neutral-400"
+          @click="showScores = !showScores"
+        >
+          Click to view scores
+        </button>
+      </div>
+
+      <div
+        v-if="showScores"
+        class="container mt-8 flex w-full flex-shrink-0 flex-row flex-wrap justify-center gap-8"
+      >
+        <div v-for="(entry, i) in scores" :key="`scores-${i}`" class="flex flex-col">
+          <span class="mb-2 self-center text-sm font-semibold">{{ entry.score }} points</span>
+          <CharacterCard
+            :character="entry.character"
+            class="w-[150px] [&_div:has(h4)]:mb-2 [&_h4]:text-lg"
+          />
+        </div>
+      </div>
+    </div>
   </Main>
 
-  <Dialog v-model="modalOpen">
+  <Dialog v-model="showModal">
     <H2 class="font-bold">Are you sure?</H2>
     <P>
       You are about to completely reset current comparison progress!
@@ -92,7 +157,7 @@ const reset = () => {
       This means, you will not be able to revert this action!
     </P>
     <div class="mt-4 flex flex-row gap-2 self-end">
-      <Button size="sm" @click="modalOpen = false">Cancel</Button>
+      <Button size="sm" @click="showModal = false">Cancel</Button>
       <Button size="sm" variant="destructive" @click="reset">Confirm</Button>
     </div>
   </Dialog>
