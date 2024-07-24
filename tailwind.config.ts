155c3aeaf613464bf4022d7a0bac186973da4c35
mk26710 <type-iv@mk26710.dev>
2024-07-24 16:43:12 +0400

    chore: configuration changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,17 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      fontFamily: {
+        anuphan: ["Anuphan", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
