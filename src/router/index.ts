45cc35a3067e8acd2eabacd85913d32e9c717f60
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-07-24 21:49:24 +0400

    feat: full compare mode page in router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import SettingsView from "@/views/SettingsView.vue";
+import FullCompareView from "@/views/FullCompareView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: "/settings",
       name: "settings",
       component: SettingsView,
+    },
+    {
+      path: "/full-compare",
+      name: "full-compate",
+      component: FullCompareView,
     },
   ],
 });
