d59733391c80311b6254fdcb1ca64da35298cd79
mk26710 <type-iv@mk26710.dev>
2024-07-24 21:38:00 +0400

    feat: secondary button variant

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -3,7 +3,7 @@ import { cn } from "@/lib/cn";
 import type { ClassValue } from "clsx";
 
 type Props = {
-  variant?: "default" | "outline";
+  variant?: "default" | "outline" | "secondary";
   size?: "default" | "sm" | "lg" | "icon";
   disabled?: boolean;
 };
@@ -28,10 +28,13 @@ const clickHandler = (e: MouseEvent) => {
   <button
     :class="
       cn(
-        'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:pointer-events-none disabled:opacity-50',
+        'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium leading-none transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:pointer-events-none disabled:opacity-50',
         props.variant === 'default' ? 'bg-neutral-200 text-neutral-800 hover:bg-neutral-400' : null,
         props.variant === 'outline'
           ? 'border border-neutral-800 bg-neutral-900 hover:bg-neutral-800'
+          : null,
+        props.variant === 'secondary'
+          ? 'bg-neutral-800 text-neutral-200 hover:bg-neutral-800/75'
           : null,
         props.size === 'default' ? 'h-9 px-4 py-2' : null,
         props.size === 'sm' ? 'h-8 px-3 text-xs' : null,
