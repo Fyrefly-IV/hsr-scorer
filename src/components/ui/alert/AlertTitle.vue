1baffeefefb0461ff52f49e8e9495511186323c4
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-07-26 01:23:42 +0400

    feat: alert component

---
+++ src/components/ui/alert/AlertTitle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { cn } from "@/lib/cn";
+import type { ClassValue } from "clsx";
+import { useAttrs } from "vue";
+
+defineOptions({ inheritAttrs: false });
+
+const attrs = useAttrs();
+</script>
+
+<template>
+  <h5
+    v-bind="{ ...attrs, class: null }"
+    :class="cn('mb-1 font-bold text-lg leading-none tracking-tight', attrs.class as ClassValue)"
+  >
+    <slot></slot>
+  </h5>
+</template>
