5d77caf0e79d63e2ac57ffc8886736aadcb7e38e
mk26710 <type-iv@mk26710.dev>
2024-07-30 18:53:17 +0400

    feat: input component

---
+++ src/components/ui/Input.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { ClassValue } from "clsx";
+import { cn } from "@/lib/cn";
+import { useAttrs } from "vue";
+
+defineOptions({
+  inheritAttrs: false,
+});
+
+const attrs = useAttrs();
+
+const model = defineModel();
+</script>
+
+<template>
+  <input
+    v-bind="{ ...attrs, class: null }"
+    v-model="model"
+    :class="
+      cn(
+        'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        attrs.class as ClassValue,
+      )
+    "
+  />
+</template>
