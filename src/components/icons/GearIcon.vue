1eca768f66ccd9f70acd56e537b93160904a597d
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-09-09 22:01:05 +0400

    fix: gear icon fill color

--- src/components/icons/GearIcon.vue
+++ src/components/icons/GearIcon.vue
@@ -11,7 +11,7 @@ const props = defineProps({
     width="15"
     height="15"
     viewBox="0 0 15 15"
-    fill="none"
+    fill="currentColor"
     xmlns="http://www.w3.org/2000/svg"
     :class="cn('size-[15px]', props.class)"
   >
