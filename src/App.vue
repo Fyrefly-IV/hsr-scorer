3d997f0548998787ebe301dfd93c874b140cd939
mk26710 <type-iv@mk26710.dev>
2024-09-10 00:00:37 +0400

    feat: replace navigation with header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import Navigation from "@/components/Navigation.vue";
+import Header from "@/components/Header.vue";
 </script>
 
 <template>
-  <Navigation />
+  <Header />
   <RouterView />
 </template>
