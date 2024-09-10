727f3b5b04b060fb2363560d99e4a4a8acd9bdca
mk26710 <type-iv@mk26710.dev>
2024-09-10 15:18:15 +0400

    feat: rearrange home screen buttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
 import Main from "@/components/Main.vue";
 import Button from "@/components/ui/Button.vue";
 import { RouterLink } from "vue-router";
-import { SettingsIcon } from "lucide-vue-next";
+import { PlayIcon, SettingsIcon } from "lucide-vue-next";
 </script>
 
 <template>
@@ -10,15 +10,18 @@ import { SettingsIcon } from "lucide-vue-next";
     <div class="flex flex-col">
       <h1 class="text-5xl font-black">Honkai Scorer</h1>
       <div class="mt-4 flex flex-col gap-2">
+        <RouterLink to="/full-compare">
+          <Button size="lg" class="w-full">
+            <PlayIcon class="h-4" />
+            <span>Play</span>
+          </Button>
+        </RouterLink>
+
         <RouterLink to="/settings">
           <Button variant="outline" size="lg" class="w-full gap-2">
             <SettingsIcon class="size-4" />
             <span>Settings</span>
           </Button>
-        </RouterLink>
-
-        <RouterLink to="/full-compare">
-          <Button size="lg" class="w-full">Full Compare</Button>
         </RouterLink>
       </div>
     </div>
