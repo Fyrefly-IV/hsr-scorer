1d81f252788fd90148d90126890426a92f8bdbc3
mk26710 <type-iv@mk26710.dev>
2024-09-17 19:37:11 +0400

    feat: more meta data about characters

--- src/entities/character/model/schemas.ts
+++ src/entities/character/model/schemas.ts
@@ -9,7 +9,28 @@ export const CharacterSchema = z.object({
   id: z.string(),
   game: GameSchema,
   name: z.string(),
-  association: z.string().nullish(),
   assets: CharacterAssetsSchema,
+  rarity: z.union([z.literal(5), z.literal(4)]),
+  association: z.string().nullish(),
 });
 
+export const StarRailCharacterSchema = CharacterSchema.extend({
+  path: z.union([
+    z.literal("Abundance"),
+    z.literal("Destruction"),
+    z.literal("Erudition"),
+    z.literal("Harmony"),
+    z.literal("Hunt"),
+    z.literal("Nihility"),
+    z.literal("Preservation"),
+  ]),
+  type: z.union([
+    z.literal("Physical"),
+    z.literal("Fire"),
+    z.literal("Ice"),
+    z.literal("Lightning"),
+    z.literal("Wind"),
+    z.literal("Quantum"),
+    z.literal("Imaginary"),
+  ]),
+});
--- src/entities/character/model/types.ts
+++ src/entities/character/model/types.ts
@@ -1,4 +1,5 @@
 import type { z } from "zod";
-import type { CharacterSchema } from "./schemas";
+import type { CharacterSchema, StarRailCharacterSchema } from "./schemas";
 
 export type Character = z.infer<typeof CharacterSchema>;
+export type StarRailCharacter = z.infer<typeof StarRailCharacterSchema>;
