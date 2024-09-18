6370b6ad946643c093f78252fc145e02bb605cd7
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-09-18 18:40:43 +0400

    feat: moved paths and types into separate schemas

--- src/entities/character/model/schemas.ts
+++ src/entities/character/model/schemas.ts
@@ -1,5 +1,14 @@
 import { z } from "zod";
 import { GameSchema } from "@/shared/model/schemas";
+import {
+  PATH_ABUNDANCE,
+  PATH_DESTRUCTION,
+  PATH_ERUDITION,
+  PATH_HARMONY,
+  PATH_HUNT,
+  PATH_NIHILITY,
+  PATH_PRESERVATION,
+} from "../config/constants";
 
 export const CharacterAssetsSchema = z.object({
   portrait: z.string().optional().nullable(),
@@ -14,23 +23,27 @@ export const CharacterSchema = z.object({
   association: z.string().nullish(),
 });
 
+export const StarRailPathsSchema = z.union([
+  z.literal(PATH_ABUNDANCE),
+  z.literal(PATH_DESTRUCTION),
+  z.literal(PATH_ERUDITION),
+  z.literal(PATH_HARMONY),
+  z.literal(PATH_HUNT),
+  z.literal(PATH_NIHILITY),
+  z.literal(PATH_PRESERVATION),
+]);
+
+export const StarRailTypesSchema = z.union([
+  z.literal("Physical"),
+  z.literal("Fire"),
+  z.literal("Ice"),
+  z.literal("Lightning"),
+  z.literal("Wind"),
+  z.literal("Quantum"),
+  z.literal("Imaginary"),
+]);
+
 export const StarRailCharacterSchema = CharacterSchema.extend({
-  path: z.union([
-    z.literal("Abundance"),
-    z.literal("Destruction"),
-    z.literal("Erudition"),
-    z.literal("Harmony"),
-    z.literal("Hunt"),
-    z.literal("Nihility"),
-    z.literal("Preservation"),
-  ]),
-  type: z.union([
-    z.literal("Physical"),
-    z.literal("Fire"),
-    z.literal("Ice"),
-    z.literal("Lightning"),
-    z.literal("Wind"),
-    z.literal("Quantum"),
-    z.literal("Imaginary"),
-  ]),
+  path: StarRailPathsSchema,
+  type: StarRailTypesSchema,
 });
