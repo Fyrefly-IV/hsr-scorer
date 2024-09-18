f794c4179293e798c3241bfb89a7d26ad60cb507
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-09-18 18:40:00 +0400

    feat: paths and types as constants

---
+++ src/entities/character/config/constants.ts
@@ -0,0 +1,35 @@
+export const PATH_ABUNDANCE = "Abundance" as const;
+export const PATH_DESTRUCTION = "Destruction" as const;
+export const PATH_ERUDITION = "Erudition" as const;
+export const PATH_HARMONY = "Harmony" as const;
+export const PATH_HUNT = "Hunt" as const;
+export const PATH_NIHILITY = "Nihility" as const;
+export const PATH_PRESERVATION = "Preservation" as const;
+
+export const TYPE_PHYSICAL = "Physical" as const;
+export const TYPE_FIRE = "Fire" as const;
+export const TYPE_ICE = "Ice" as const;
+export const TYPE_LIGHTNING = "Lightning" as const;
+export const TYPE_WIND = "Wind" as const;
+export const TYPE_QUANTUM = "Quantum" as const;
+export const TYPE_IMAGINARY = "Imaginary" as const;
+
+export const CHARACTER_PATHS = [
+  PATH_ABUNDANCE,
+  PATH_DESTRUCTION,
+  PATH_ERUDITION,
+  PATH_HARMONY,
+  PATH_HUNT,
+  PATH_NIHILITY,
+  PATH_PRESERVATION,
+] as const;
+
+export const CHARACTER_TYPES = [
+  TYPE_PHYSICAL,
+  TYPE_FIRE,
+  TYPE_ICE,
+  TYPE_LIGHTNING,
+  TYPE_WIND,
+  TYPE_QUANTUM,
+  TYPE_IMAGINARY,
+] as const;
