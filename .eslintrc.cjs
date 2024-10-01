96b34bdcd4171141960b83ea72d03ffc6fe6553b
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-10-01 23:23:38 +0400

    chore: disable conflicting rules with prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,8 @@ module.exports = {
   },
   rules: {
     "vue/multi-word-component-names": "off",
+    "vue/html-self-closing": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/singleline-html-element-content-newline": "off",
   },
 };
