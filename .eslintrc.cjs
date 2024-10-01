794dfb48e50a03c660914724ad4eec76eca0958e
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-10-01 22:21:17 +0400

    fix: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,19 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-env node */
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  plugins: ["@typescript-eslint"],
   extends: [
-    "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier/skip-formatting",
     "@feature-sliced",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
   },
   rules: {
     "vue/multi-word-component-names": "off",
