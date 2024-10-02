a0c39966d9cecc64f941c142e1a4535955505e43
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-10-02 22:14:53 +0400

    refactor: character card should be more semantically correct

--- src/entities/character/ui/CharacterCard.vue
+++ src/entities/character/ui/CharacterCard.vue
@@ -7,10 +7,6 @@ type Props = {
   character: StarRailCharacter;
   hideName?: boolean;
   textUnderName?: string;
-  excluded?: boolean;
-  hoverable?: boolean;
-  width?: number;
-  height?: number;
   decoration?: boolean;
   showType?: boolean;
   showPath?: boolean;
@@ -18,58 +14,21 @@ type Props = {
 };
 
 const props = withDefaults(defineProps<Props>(), {
-  width: 200,
-  height: 300,
   textUnderName: undefined,
   class: "",
 });
-
-type Emits = {
-  click: [payload?: MouseEvent];
-};
-
-const emit = defineEmits<Emits>();
-
-const clickHandler = (e: MouseEvent) => {
-  emit("click", e);
-};
 </script>
 
 <template>
-  <figure
-    :class="
-      cn(
-        'group relative overflow-hidden rounded-lg border border-border bg-secondary/30 transition-transform data-[excluded=true]:translate-y-2 data-[excluded=true]:opacity-30',
-        props.class,
-      )
-    "
-    :data-excluded="excluded"
-    :data-hoverable="hoverable"
-    @click="clickHandler"
-  >
+  <article :class="cn('relative overflow-hidden rounded-md border border-border', props.class)">
     <img
       :src="character.assets.portrait ?? undefined"
-      :style="{ aspectRatio: `${width}/${height}` }"
-      class="h-full w-full object-cover transition-transform duration-300 ease-in-out group-data-[hoverable=true]:hover:scale-105"
+      class="card-thumbnail aspect-[376/512] h-full w-full object-cover"
       draggable="false"
       decoding="async"
       loading="eager"
     />
-    <figcaption
-      v-if="hideName !== true"
-      class="absolute inset-x-0 bottom-0 flex flex-col items-center rounded-b-md bg-gradient-to-b from-black/40 to-black/60 px-4 pb-3 pt-2 text-center text-white"
-    >
-      <h3 class="relative w-fit font-anuphane text-lg font-semibold">
-        <SparklesIcon
-          v-if="decoration === true"
-          class="absolute -right-3 -top-1 size-4 -scale-x-100 scale-y-100 stroke-emerald-300 opacity-75"
-        />
-        <span>{{ character.name }}</span>
-      </h3>
-      <p v-if="typeof textUnderName === 'string'" class="font-anuphane text-sm">
-        {{ textUnderName }}
-      </p>
-    </figcaption>
+
     <div
       v-if="showPath === true || showType === true"
       class="absolute left-0 top-0 flex w-full justify-between px-1 pt-1 [&_img]:drop-shadow-[0_1px_1px_#000]"
@@ -85,5 +44,34 @@ const clickHandler = (e: MouseEvent) => {
         class="aspect-square size-8"
       />
     </div>
-  </figure>
+
+    <div
+      v-if="hideName !== true"
+      class="absolute bottom-0 left-0 right-0 flex w-full flex-col items-center bg-gradient-to-b from-black/40 to-black/60 pb-3.5 pt-3 backdrop-blur-sm has-[p]:pb-2.5 has-[p]:pt-2"
+    >
+      <h3 class="relative font-anuphane text-lg font-semibold leading-none text-white">
+        <SparklesIcon
+          v-if="decoration"
+          class="absolute -left-2.5 -top-2 h-4 w-4 text-emerald-400 opacity-75"
+        />
+        <span>{{ character.name }}</span>
+      </h3>
+      <p v-if="typeof textUnderName === 'string' && textUnderName.length > 0">
+        {{ textUnderName }}
+      </p>
+    </div>
+  </article>
 </template>
+
+<style lang="css" scoped>
+button .card-thumbnail {
+  transition-property: transform;
+  transition-duration: 300ms;
+  transition-timing-function: cubic-bezier(0, 0, 0.2, 1);
+}
+
+button:hover .card-thumbnail,
+button:active .card-thumbnail {
+  transform: scale(1.05);
+}
+</style>
