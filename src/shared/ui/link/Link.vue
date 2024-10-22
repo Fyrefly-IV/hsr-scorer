a3f356a3acced708a601db576a3849daf3a41d30
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2024-10-22 20:40:17 +0400

    feat: external link component

---
+++ src/shared/ui/link/Link.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { cn } from "@/shared/lib/cn";
+
+type Props = {
+  href: HTMLAnchorElement["href"];
+  target?: HTMLAnchorElement["target"];
+  class?: string;
+};
+
+const { href, target = "_blank", ...props } = defineProps<Props>();
+</script>
+
+<template>
+  <a
+    :href="href"
+    :target="target"
+    rel="noreferrer"
+    :class="cn('font-medium underline underline-offset-2', props.class)"
+  >
+    <slot></slot>
+  </a>
+</template>
