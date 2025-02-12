598140bae9849d80f7ecd141c837e671998bb862
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2025-02-13 01:30:29 +0400

    feat: hide footer in game on mobile screens

--- src/widgets/footer/ui/Footer.vue
+++ src/widgets/footer/ui/Footer.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { IN_PROGRESS } from "@/features/game/config";
+import { useGame } from "@/features/game/model/store";
 import Link from "@/shared/ui/link/Link.vue";
+
+const game = useGame();
 </script>
 
 <template>
-	<footer class="border-border mt-6 border-t">
+	<footer
+		:data-gaming="game.stage === IN_PROGRESS"
+		class="border-border mt-6 border-t max-lg:data-[gaming=true]:sr-only"
+	>
 		<div class="border-border container py-6">
 			<p class="text-muted-foreground text-center text-sm text-balance md:text-left">
 				Built by a Honkai Star Rail player! The source code is available on
