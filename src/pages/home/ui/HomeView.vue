936f8831f3d8fa80083ea12d21947639ae7d4f1c
Fyrefly-IV <184772939+Fyrefly-IV@users.noreply.github.com>
2025-02-16 02:10:27 +0400

    feat: jump straight into the game via modal on home page

--- src/pages/home/ui/HomeView.vue
+++ src/pages/home/ui/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { MoonIcon, PlayIcon, Settings2Icon, SunIcon } from "lucide-vue-next";
-import { RouterLink } from "vue-router";
+import { RouterLink, useRouter } from "vue-router";
 import Button from "@/shared/ui/button/Button.vue";
 import CardHeader from "@/shared/ui/card/CardHeader.vue";
 import CardTitle from "@/shared/ui/card/CardTitle.vue";
@@ -8,12 +8,31 @@ import CardContent from "@/shared/ui/card/CardContent.vue";
 import CardFooter from "@/shared/ui/card/CardFooter.vue";
 import Card from "@/shared/ui/card/Card.vue";
 import { useThemeStore } from "@/features/theme-switch/model/theme.store";
+import H2 from "@/shared/ui/typography/H2.vue";
+import P from "@/shared/ui/typography/P.vue";
+import { ref } from "vue";
+import Dialog from "@/shared/ui/dialog/Dialog.vue";
+import { useGame } from "@/features/game/model/store";
 
+const game = useGame();
 const theme = useThemeStore();
+const router = useRouter();
+
+const startModalOpen = ref(false);
+
+function start() {
+	try {
+		game.start();
+		router.push("/play");
+	} catch (e) {
+		alert("error!");
+		console.error(e);
+	}
+}
 </script>
 
 <template>
-	<div class="bg-background text-foreground min-h-svh-main flex items-center justify-center p-4">
+	<main class="bg-background text-foreground h-svh-main flex items-center justify-center p-4">
 		<Card class="w-full max-w-md">
 			<CardHeader>
 				<CardTitle class="text-center text-4xl font-bold">Honkai Scorer</CardTitle>
@@ -22,12 +41,10 @@ const theme = useThemeStore();
 				<p class="text-muted-foreground text-center">
 					Compare and rank your favorite Honkai Star Rail characters in this strategic showdown.
 				</p>
-				<RouterLink to="/play">
-					<Button size="lg" class="w-full">
-						<PlayIcon class="mr-2 h-4 w-4" />
-						<span>Play Now</span>
-					</Button>
-				</RouterLink>
+				<Button size="lg" class="w-full" @click="startModalOpen = true">
+					<PlayIcon class="mr-2 h-4 w-4" />
+					<span>Play Now</span>
+				</Button>
 			</CardContent>
 			<CardFooter class="flex justify-between">
 				<Button variant="outline" size="icon" @click="theme.switchTheme">
@@ -46,5 +63,17 @@ const theme = useThemeStore();
 				</RouterLink>
 			</CardFooter>
 		</Card>
-	</div>
+	</main>
+
+	<Dialog v-model="startModalOpen">
+		<H2 class="font-bold">We're about to start!</H2>
+		<P class="text-prettyp">
+			The process of comparing many characters might take a while!<br />
+			Don't worry, you can close the tab and come back to comparing whenever you feel like it!
+		</P>
+		<div class="mt-2 flex gap-2 self-end">
+			<Button variant="outline" @click="startModalOpen = false">Cancel</Button>
+			<Button variant="default" @click="start()">Proceed</Button>
+		</div>
+	</Dialog>
 </template>
